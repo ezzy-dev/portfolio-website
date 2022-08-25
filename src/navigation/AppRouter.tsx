e3ea72c3a44b6ecdb0c0dea9e480bc8337cbbbfa
Ezra <ezra.furry@gmail.com>
2022-08-25 13:31:41 -0400

    updated home page

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
 
 function AppRouter(): JSX.Element {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
-  );
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      );
 }
 
 export default AppRouter;
+
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,8 @@ import React from "react";
 function Home(): JSX.Element {
     return (
         <div className="home-container">
-            <h1>Home</h1>
+            <h1>Jordan Myers Portfolio Website</h1>
+            <h2>A project for CS5167 - User Interface I</h2>
         </div>
     );
 };
