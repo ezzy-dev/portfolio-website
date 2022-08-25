0fdf73bdbb8acb11f9428e11087dbee0455aa98d
Ezra <ezra.furry@gmail.com>
2022-08-25 13:33:44 -0400

    fixed issue with BrowserRouter

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
 
 function AppRouter(): JSX.Element {
     return (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        </Routes>
       );
 }
 
