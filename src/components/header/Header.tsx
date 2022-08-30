c85c1e6664f1c6b16595bcc8828c302a0be8acce
ezra <ezra.furry@gmail.com>
2022-08-30 15:30:13 -0400

    basic wip header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.scss"
 
 function Header(): JSX.Element {
     return (
         <header>
             <nav className="header-container">
-                <Link to="/">Home</Link>
+                <div className="header-left">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="header-right">
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
             </nav>
         </header>
     );
